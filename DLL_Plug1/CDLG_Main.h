e895447b256fefec1dc535446a435e8ec5a0398d
mhws2 <mhws2@MHWS2>
2020-12-15 23:28:16 +0800

    更新MFC插件

---
+++ DLL_Plug1/CDLG_Main.h
@@ -0,0 +1,25 @@
+﻿#pragma once
+#include "afxdialogex.h"
+#include "resource.h"
+
+
+// CDLG_Main 对话框
+
+class CDLG_Main : public CDialogEx
+{
+	DECLARE_DYNAMIC(CDLG_Main)
+
+public:
+	CDLG_Main(CWnd* pParent = nullptr);   // 标准构造函数
+	virtual ~CDLG_Main();
+
+// 对话框数据
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_DIALOG1 };
+#endif
+
+protected:
+	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV 支持
+
+	DECLARE_MESSAGE_MAP()
+};
